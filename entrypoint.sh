c2400389758f9f64f834f419bf57192140910a3a
Tonye Jack <jtonye@ymail.com>
2020-05-22 21:23:57 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@ set -e
 
 CONFIG_PATH=$1
 IGNORE_PATH=$2
-CHANGED_FILES := $(git diff --diff-filter=ACM --name-only $(git merge-base origin/develop HEAD) | grep -E ".(js|jsx|ts|tsx)$$")
+TARGET_BRANCH=$3
+
+git fetch --depth=1 origin +refs/heads/$TARGET_BRANCH:refs/remotes/origin/$TARGET_BRANCH
+
+CHANGED_FILES=$(git diff --diff-filter=ACM --name-only $(git merge-base origin/develop HEAD) | grep -E ".(js|jsx|ts|tsx)$$")
 
 echo "## Running ESLint"
 eslint --config=$CONFIG_PATH --ignore-path $IGNORE_PATH --max-warnings=0 $(CHANGED_FILES)
