5e9f901a18c997a017a3aca29bd7b1677df61c42
Tonye Jack <jtonye@ymail.com>
2020-05-23 08:26:32 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,17 @@
 
 set -e 
 
-CONFIG_PATH=$1
-IGNORE_PATH=$2
-TARGET_BRANCH=$3
+CONFIG_PATH=$INPUT_CONFIG-PATH
+IGNORE_PATH=$INPUT_IGNORE-PATH
+TARGET_BRANCH=$GITHUB_BASE_REF
 
 git fetch --depth=1 origin +refs/heads/$TARGET_BRANCH:refs/remotes/origin/$TARGET_BRANCH
 
-CHANGED_FILES=$(git diff --diff-filter=ACM --name-only $(git merge-base origin/develop HEAD) | grep -E ".(js|jsx|ts|tsx)$$")
+CHANGED_FILES=$(git diff --diff-filter=ACM --name-only $(git merge-base origin/$TARGET_BRANCH HEAD) | grep -E ".(js|jsx|ts|tsx)$$")
 
 echo "## Running ESLint"
-eslint --config=$CONFIG_PATH --ignore-path $IGNORE_PATH --max-warnings=0 $(CHANGED_FILES)
+if [[ ! -z $IGNORE_PATH ]]; then
+  eslint --config=$CONFIG_PATH --ignore-path $IGNORE_PATH --max-warnings=0 $(CHANGED_FILES)
+else
+  eslint --config=$CONFIG_PATH --max-warnings=0 $(CHANGED_FILES)
+fi
