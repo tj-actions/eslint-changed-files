73d2110df663591626b3d92d84d4edcdbe0517c9
Tonye Jack <jtonye@ymail.com>
2020-05-23 08:39:23 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,12 @@ CONFIG_PATH=$INPUT_CONFIG-PATH
 IGNORE_PATH=$INPUT_IGNORE-PATH
 TARGET_BRANCH=$GITHUB_BASE_REF
 
+
+echo ${INPUT_CONFIG-PATH}
+echo ${INPUT_IGNORE-PATH}
+echo ${GITHUB_BASE_REF}
+env
+
 git fetch --depth=1 origin +refs/heads/$TARGET_BRANCH:refs/remotes/origin/$TARGET_BRANCH
 
 CHANGED_FILES=$(git diff --diff-filter=ACM --name-only $(git merge-base origin/$TARGET_BRANCH HEAD) | grep -E ".(js|jsx|ts|tsx)$$")
