6a244d2ed3b68383be67f1ffeea41888abb42708
Tonye Jack <jtonye@ymail.com>
2021-03-07 09:11:05 -0500

    Update App.tsx

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" >
+          
+          
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
