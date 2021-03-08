eeb06f7f4eb6b7c43a81c8564ba393ce767bf159
Tonye Jack <jtonye@ymail.com>
2021-03-08 00:04:49 -0500

    Update index.tsx

--- test-app/src/index.tsx
+++ test-app/src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  <React.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
