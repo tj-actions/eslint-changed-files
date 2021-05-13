f15b26c7fa810625b4605418a38ab9814746d203
Tonye Jack <jtonye@ymail.com>
2021-05-13 19:31:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ EXTENSIONS=${4// /}
 EXTRA_ARGS=$5
 EXCLUDED=()
 
-IFS=" " read -r -a EXCLUDED <<< $(echo "$6" | tr "\n" " ")
+IFS=" " read -r -a EXCLUDED <<< "$(echo "$6" | tr "\n" " ")"
 
 TARGET_BRANCH=${GITHUB_BASE_REF}
 
