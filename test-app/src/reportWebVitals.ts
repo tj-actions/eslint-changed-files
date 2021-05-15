e495edf49162153282c100e10a250a2ec6ea77dc
Tonye Jack <jtonye@ymail.com>
2021-05-14 20:18:47 -0400

    Update reportWebVitals.ts

--- test-app/src/reportWebVitals.ts
+++ test-app/src/reportWebVitals.ts
@@ -1,5 +1,7 @@
 import { ReportHandler } from 'web-vitals';
 
+console.log("Foo");
+
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
