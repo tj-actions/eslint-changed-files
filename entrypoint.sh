80069d295bbb031ae35030eb3874f77d838a8929
Tonye Jack <jtonye@ymail.com>
2021-05-26 19:52:52 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ if [[ -n "${EXCLUDED[*]}" && -n "${MODIFIED_FILES[*]}" ]]; then
 
   for changed_file in "${MODIFIED_FILES[@]}"
   do
-    if $(echo "$changed_file" | grep -qiE "($EXCLUDED_REGEX)"; then
+    if echo "$changed_file" | grep -qiE "($EXCLUDED_REGEX)"; then
       FILE+=("$changed_file")
     fi
   done
