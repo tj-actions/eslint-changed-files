fc257334889baeb96cfe1893f54b18c5c8b0b55e
Tonye Jack <jtonye@ymail.com>
2021-09-06 12:03:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,18 +81,6 @@ else
   IFS=" " read -r -a FILES <<< "$(echo "${MODIFIED_FILES[*]}" | xargs)"
 fi
 
-
-TEMP_PATH="$(mktemp -d)"
-PATH="${TEMP_PATH}:$PATH"
-REVIEWDOG_VERSION="v0.13.0"
-ESLINT_FORMATTER="${GITHUB_ACTION_PATH}/formatter.js"
-
-export REVIEWDOG_GITHUB_API_TOKEN="${GITHUB_TOKEN}"
-
-echo '🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
-
-
 if [[ -n "${FILES[*]}" ]]; then
   echo ""
   echo "Changed files"
