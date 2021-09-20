123a71b6dac021a1b99d27c6221f9a5fb320b9cc
Tonye Jack <jtonye@ymail.com>
2021-09-20 15:21:51 -0400

    Update App.tsx

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -3,9 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo1" />
         <p>
