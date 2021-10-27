442e938e426f0615d2871d8b4ef21725257644ee
Tonye Jack <jtonye@ymail.com>
2021-10-27 07:33:18 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -124,7 +124,7 @@ if [[ -n "${FILES[*]}" ]]; then
     if [[ ! -z ${IGNORE_PATH} ]]; then
       echo "Using ignore path: $IGNORE_PATH"
       # shellcheck disable=SC2086
-      npx eslint --config="${CONFIG_PATH}" --ignore-path "${IGNORE_PATH}" ${EXTRA_ARGS} -f="${ESLINT_FORMATTER}" $CHANGED_FILES | reviewdog -f=rdjson \
+      npx eslint --config="${CONFIG_PATH}" --ignore-path="${IGNORE_PATH}" ${EXTRA_ARGS} -f="${ESLINT_FORMATTER}" $CHANGED_FILES | reviewdog -f=rdjson \
         -name=eslint \
         -reporter=github-pr-review \
         -filter-mode=nofilter \
