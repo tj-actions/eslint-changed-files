6656102aaa2bd15fec7289fe56d07397eccb09af
Tonye Jack <jtonye@ymail.com>
2022-03-09 22:29:45 -0500

    Update App.test.tsx

--- test-app/src/App.test.tsx
+++ test-app/src/App.test.tsx
@@ -3,8 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App>);
   const linkElement = screen.getByText(/learn react/i);
-  console.error("Test");
   expect(linkElement).toBeInTheDocument();
 });
