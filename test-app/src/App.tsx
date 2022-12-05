a184518a9179c43d1b73c1c9068f6c842d3eb61e
Tonye Jack <jtonye@ymail.com>
2022-12-04 22:10:13 -0700

    Update test-app/src/App.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App () : JSX.Element {
+function App (): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
