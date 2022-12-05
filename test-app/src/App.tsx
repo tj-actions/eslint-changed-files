592dfb56e0bfb41e37d1a108a77f7d9997b60d61
Tonye Jack <jtonye@ymail.com>
2022-12-04 22:05:16 -0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
