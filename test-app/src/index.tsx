b2d85a4634f7325ae07edde5a29be4cd2648ccf3
Tonye Jack <jtonye@ymail.com>
2023-01-13 12:54:28 -0700

    Update index.tsx

--- test-app/src/index.tsx
+++ test-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
