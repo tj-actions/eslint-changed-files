64f1d670a6a545d387438e94f5bd9da90d114a15
Tonye Jack <jtonye@ymail.com>
2023-05-26 09:21:58 -0600

    chore: Update App.tsx

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -6,7 +6,7 @@ function App (): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo1" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
