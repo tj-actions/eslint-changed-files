f1583fd2af2a6c830abd1107cee0c7b475736a0b
Tonye Jack <jtonye@ymail.com>
2023-07-02 18:29:57 -0600

    Update .eslintrc.js

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: path.join(__dirname, 'tsconfig.json')
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: [
     'react'
