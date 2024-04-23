a73f9ebde3056521d0d2daa5b08f92c83c7959eb
Tonye Jack <jtonye@ymail.com>
2024-04-23 10:30:37 -0600

    Fix lint errors

--- test-app/eslint.config.mjs
+++ test-app/eslint.config.mjs
@@ -1,17 +1,17 @@
 import globals from "globals";
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
 import styleLintConfig from "stylelint-config-recommended";
-import prettierConfig from 'eslint-config-prettier';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettierConfig from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended
+  recommendedConfig: pluginJs.configs.recommended,
 });
 
 export default [
@@ -19,19 +19,17 @@ export default [
     languageOptions: { globals: globals.browser },
     settings: { react: { version: "detect" } },
   },
-  ...compat.extends("standard-with-typescript").map(
-    config => ({
-      ...config,
-      files: ["**/*.tsx", "**/*.ts"],
-      rules: {
-        ...config.rules,
-        "@typescript-eslint/semi": ["error", "always"],
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-      }
-    })
-  ),
+  ...compat.extends("standard-with-typescript").map((config) => ({
+    ...config,
+    files: ["**/*.tsx", "**/*.ts"],
+    rules: {
+      ...config.rules,
+      "@typescript-eslint/semi": ["error", "always"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  })),
   prettierConfig, // Turns off all ESLint rules that have the potential to interfere with Prettier rules.
   eslintPluginPrettierRecommended,
   {
--- test-app/src/reportWebVitals.ts
+++ test-app/src/reportWebVitals.ts
@@ -1,17 +1,15 @@
 import { type ReportHandler } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-    if (onPerfEntry != null && onPerfEntry instanceof Function) {
-        import("web-vitals").then(
-            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-                getCLS(onPerfEntry);
-                getFID(onPerfEntry);
-                getFCP(onPerfEntry);
-                getLCP(onPerfEntry);
-                getTTFB(onPerfEntry);
-            },
-        );
-    }
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
 };
 
 export default reportWebVitals;
